f3efc11487374297d64f130135e1f4816c9359ae
Rawlings Okigbo <58330958+rollex6@users.noreply.github.com>
2022-04-22 18:15:14 +0100

    training daces and identification
    add a folder (the name of the folder sould be your name) of 5 of your images (head shots of yourself and only your face must be in the picture)
    run faces-train.py to train the data
    then run faces.py to test your face

--- src/faces.py
+++ src/faces.py
@@ -1,7 +1,16 @@
+import numpy as np
 import cv2
+import pickle
 
 # read face classifier file
 faceCascade = cv2.CascadeClassifier(r"cascades\data\haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainner.yml')
+
+labels = {}
+with open('labels.pickle', 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k, v in og_labels.items()}
 
 video_capture = cv2.VideoCapture(0)
 
@@ -16,9 +25,19 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        print(x, y, w, h)
+        #print(x, y, w, h)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>= 45 and conf <= 85:
+            print(id_)
+            print (labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 0, 0)
+            stroke = 1
+            cv2.putText(frame, name, (x,y), font, 0.5, color, stroke, cv2.LINE_AA)
+
         img_item = 'test_image.png'
         img_item2 = 'test_image2.jpeg'
         cv2.imwrite(img_item, roi_gray)
@@ -26,9 +45,10 @@ while True:
 
         cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0))
 
-    # sDisplay resulting frame
-    cv2.imshow('gray', gray)
 
+
+    # sDisplay resulting frame
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 print("Face count is: ", len(faces))
